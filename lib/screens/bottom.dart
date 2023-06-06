526cbd8b1f3ee78588c9eff005df23cbfdff2f8d
VishSaurbh <100016387+VishSaurbh@users.noreply.github.com>
2023-06-06 23:09:14 +0530

    first-commit BottomNavigationBar Added

---
+++ lib/screens/bottom.dart
@@ -0,0 +1,59 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({super.key});
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _selectIndex = 0;
+  static final List<Widget>_widgetOptions =<Widget>[
+    const Text("Home"),
+    const Text("Search"),
+    const Text("Tickets"),
+    const Text("Profile")
+  ];
+  void _onItemTapped(int index){
+    setState(() {
+      _selectIndex=index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My tickets")
+      ),
+      body: Center(
+        child: _widgetOptions[_selectIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectIndex,
+        onTap: _onItemTapped,
+        elevation: 10,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: const Color(0xFF526480),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+              label: "Home"),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              label: "Search"),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+          label: "Tickets"),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_person_add_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+              label: "Profile")
+        ],
+      ),
+    );
+  }
+}
